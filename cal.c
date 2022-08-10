82b4d77b181b4c39e527b0f00106ee2b22a12d70
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-08-09 22:48:37 -0500

    Added comments.

--- cal.c
+++ cal.c
@@ -11,13 +11,22 @@ char *months[] = {"January",   "February", "March",    "April",
 int main() {
   int duration = 100;
 
+  /*
+   * Get initial values for the current time.
+   */
   time_t current_time = time(0);
   struct tm *curr = localtime(&current_time);
   int current_mday = curr->tm_mday;
   int current_mon = curr->tm_mon;
 
+  /*
+   * Pick a point in the past to start from.
+   */
   time_t initial_time = current_time - duration / 2 * ONEDAY;
 
+  /*
+   * Print a header.
+   */
   printf("Su Mo Tu We Th Fr Sa\n");
   printf("--------------------\n");
 
@@ -31,13 +40,23 @@ int main() {
   int beginning_of_month = 1;
   for (int i = 0; i < duration; i++) {
 
+    /*
+     * Get the selected date for this iteration of the for loop.
+     */
     time_t t = initial_time + i * ONEDAY;
     struct tm *tm = localtime(&t);
 
+    /*
+     * Days that are numbered less than "10" need additional padding.
+     */
     if (tm->tm_mday < 10) {
       printf(" ");
     }
 
+    /*
+     * Print out the currently selected date. If the value matches the current
+     * actual date, print it out in color.
+     */
     if (tm->tm_mday == current_mday && tm->tm_mon == current_mon) {
       printf("\033[1;33m");
       printf("%d ", tm->tm_mday);
@@ -46,6 +65,9 @@ int main() {
       printf("%d ", tm->tm_mday);
     }
 
+    /*
+     * End the line on Saturday. Also print out the month name if applicable.
+     */
     if (tm->tm_mday == 1) {
       beginning_of_month = 1;
     }
