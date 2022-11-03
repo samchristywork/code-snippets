f0eb70b0809d343af93a16e48397970e58b9d06b
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-11-03 17:12:19 -0500

    Feature: Take filename as command line arg
    Parses the command line arguments supplied by the user and uses the
    first one as the filename of the Rust source to be parsed instead of
    relying on a hard-coded value.
    
    Contact: Sam Christy <samchristywork@gmail.com>

--- rust_ast/src/main.rs
+++ rust_ast/src/main.rs
@@ -1,8 +1,21 @@
+use std::env;
 use std::fs::File;
 use std::io::Read;
+use std::process;
 
 fn main() {
-    let mut file = File::open("src/main.rs").expect("Unable to open file");
+    let mut args = env::args();
+    let program_name = args.next().unwrap();
+
+    let filename = match (args.next(), args.next()) {
+        (Some(filename), None) => filename,
+        _ => {
+            eprintln!("Usage: {} path/to/filename.rs", program_name);
+            process::exit(1);
+        }
+    };
+
+    let mut file = File::open(&filename).expect("Unable to open file");
 
     let mut src = String::new();
     file.read_to_string(&mut src).expect("Unable to read file");
