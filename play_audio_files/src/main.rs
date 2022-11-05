31fc2b1427394347f7eefc810ebf50cb7a1cf51a
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-11-04 21:52:28 -0500

    Feature: Program now takes a filename as argument
    Contact: Sam Christy <samchristywork@gmail.com>

--- play_audio_files/src/main.rs
+++ play_audio_files/src/main.rs
@@ -1,11 +1,24 @@
 use soloud::*;
+use std::env;
+use std::process;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut args = env::args();
+    let program_name = args.next().unwrap();
+
+    let filename = match (args.next(), args.next()) {
+        (Some(filename), None) => filename,
+        _ => {
+            eprintln!("Usage: {} path/to/filename.rs", program_name);
+            process::exit(1);
+        }
+    };
+
     let sl = Soloud::default()?;
 
     let mut wav = audio::Wav::default();
 
-    wav.load(&std::path::Path::new("path/to/filename.wav"))?;
+    wav.load(&std::path::Path::new(&filename))?;
 
     sl.play(&wav);
     while sl.voice_count() > 0 {
