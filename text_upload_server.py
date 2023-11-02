479875eb77bcc6a8902ff3ecf0e598797c01024b
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-11-02 17:25:01 -0500

    feat: Add simple text upload server
    The server currently just hosts an HTML page, but doesn't yet save the
    text.

---
+++ text_upload_server.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        text = request.form.get('text_area')
+        return 'Text received.'
+    return '''
+    <form method="post">
+        <textarea name="text_area" rows="10" cols="50"></textarea><br>
+        <input type="submit" value="Submit">
+    </form>
+    '''
+
+if __name__ == '__main__':
+    app.run(debug=True)
