b0fc622768ac56b5eaedcd2e079dd2f79cee1303
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-11-19 22:11:32 -0600

    feat: Add a route that sets the global data
    This route allows the client application to set the data stored on the
    server.

--- code_snippet_server/main.py
+++ code_snippet_server/main.py
@@ -2,9 +2,18 @@ from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
+submitted_data = "Initial data"
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/data', methods=['POST'])
+def data():
+    global submitted_data
+    json_data = request.json
+    submitted_data = json_data.get('text', submitted_data)
+    return jsonify({"text": submitted_data})
 
 if __name__ == '__main__':
     app.run(debug=True)
